5722954aa25b4e44b80420102bb589d4491ac0f6
m_hagiwara <masahiro.hagiwara@acreation.co.jp>
2016-02-29 07:47:17 +0000

    first commit user-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,10 +18,18 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+#    @user = User.find(params[:id])
+  end
+ 
+  def update
+#    @user = User.find(params[:id])
+  end
+ 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :area)
   end
+
 end
