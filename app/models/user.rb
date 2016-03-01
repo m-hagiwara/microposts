40d51bdf42e89aac265cb47f43e96c7e92b98ad3
m_hagiwara <masahiro.hagiwara@acreation.co.jp>
2016-03-01 08:48:35 +0000

    add user-profile 20160301-1

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :area , length: { minimum: 0, maximum: 10 } , presence: true
+  validates :area , length: { minimum: 0, maximum: 10 } , presence: true, on: :update
   validates :telno, numericality: {
-    only_integer: true
+    only_integer: true, on: :update
   }
 end
