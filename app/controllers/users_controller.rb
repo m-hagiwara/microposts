a4809f0c37844283442c9a893acd503d3980e55e
m_hagiwara <masahiro.hagiwara@acreation.co.jp>
2016-03-04 10:01:56 +0000

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
-  before_action :collect_user, only: [:edit, :update] 
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+                                        :following, :followers]
 
   def show # 追加
    @user = User.find(params[:id])
@@ -34,17 +35,30 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
- 
+
+  def followings
+    @title = "Followings"
+    @user  = User.find(params[:id])
+#   @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+  
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+#   @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
   private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :area, :teln)
-  end
-
-  def collect_user
-    user = User.find(params[:id])
-    if !current_user?(user)
-      redirect_to(root_url)
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :area, :teln)
     end
-  end
+
+    def collect_user
+      user = User.find(params[:id])
+      if !current_user?(user)
+        redirect_to(root_url)
+      end
+    end
 end
--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Microposts
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # Thqe default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
@@ -29,5 +29,7 @@ module Microposts
     g.helper false
     g.test_framework false
     end
+    
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
