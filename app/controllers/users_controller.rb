66c2e7df4173b4fb802c0857c1249a8753a2e683
m_hagiwara <masahiro.hagiwara@acreation.co.jp>
2016-03-10 08:38:15 +0000

    followings-followers add 20160310

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
 
-　before_action :collect_user, only: [:edit, :update]
+  before_action :collect_user, only: [:edit, :update]
 
   def show # 追加
-   @user = User.find(params[:id])
-   @microposts = @user.microposts.order(created_at: :desc)
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.order(created_at: :desc)
   end
   
   def new
